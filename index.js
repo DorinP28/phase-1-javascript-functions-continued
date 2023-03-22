894d33946d0f775937e713e0f4fbce1c5599294c
Dorin Porcereanu <porcereanu.dorin@gmail.com>
2023-03-21 22:43:27 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// code your solution here
+const saturdayFun = (activity = "roller-skate") => {
+    console.log(`This Saturday, I want to ${activity}!`);
+    return `This Saturday, I want to ${activity}!`;
+  };
+
+  const mondayWork = (activity = "go to the office") => {
+    console.log(`This Monday, I will ${activity}.`);
+    return `This Monday, I will ${activity}.`;
+  };
+
+  function wrapAdjective(star = "*") {
+    return function(adjective = "special") {
+      return `You are ${star}${adjective}${star}!`
+    }
+  }
+
+const nicely = wrapAdjective();
+console.log(nicely());
